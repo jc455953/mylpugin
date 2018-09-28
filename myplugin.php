61cfba09e3d38f272f4fa36ca8a6293abebc8c6c
jc455953 <ramandeepsingh.dhillon@my.jcu.edu.au>
2018-09-28 10:40:53 +1000

    Add files via upload

---
+++ myplugin.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Plugin Name: myplugin
+Description: Welcome to WordPress plugin development.
+Plugin URI:  https://plugin-planet.com/
+Author:      ramandeep dhillon
+
+Version:     1.0
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+
+
+// if admin area
+if ( is_admin() ) {
+
+	// include dependencies
+	require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
+	require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+        require_once plugin_dir_path( __FILE__ ) . 'admin/settings-register.php';
+        require_once plugin_dir_path( __FILE__ ) . 'admin/settings-callbacks.php';
+        require_once plugin_dir_path( __FILE__ ) . 'admin/settings-validate.php';
+
+
+}
+
+// default plugin options
+function myplugin_options_default() {
+
+	return array(
+		'custom_url'     => 'https://wordpress.org/',
+		'custom_title'   => 'Powered by WordPress',
+		'custom_style'   => 'disable',
+		'custom_message' => '<p class="custom-message">My custom message</p>',
+		'custom_footer'  => 'Special message for users',
+		'custom_toolbar' => false,
+		'custom_scheme'  => 'default',
+	);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
